6e91b783881dfe2d0396005d649e8b3944791b85
irving <chenghuanl@genvis.co>
2018-10-09 11:25:25 +0800

    skip existing files.

--- parallel_download.py
+++ parallel_download.py
@@ -18,15 +18,18 @@ with open('car_sample.pickle', 'rb') as f:
 custom_samples = body_face_samples.copy()
 custom_samples.update(car_samples)
 
-download_path = [sample.remote_path for key, sample in custom_samples.items()]
+
+iter_list = [(key, sample.remote_path) for key, sample in custom_samples.items()]
 
 
 # print(download_path)
 # print(len(download_path))
 
-def download(remote_path):
-    print('wget ' + remote_path + ' -P ' + ProjectSettings.instance().CUSTOM_STORAGE_DIRECTORY)
-    os.system('wget ' + remote_path + ' -P ' + ProjectSettings.instance().CUSTOM_STORAGE_DIRECTORY)
+def download(iter):
+    print(iter[0], 'wget ' + iter[1] + ' -P ' + ProjectSettings.instance().CUSTOM_STORAGE_DIRECTORY)
+    if not os.path.exists(os.path.join(ProjectSettings.instance().CUSTOM_STORAGE_DIRECTORY,
+                                       iter[0] + '.jpg')):
+        os.system('wget ' + iter[1] + ' -P ' + ProjectSettings.instance().CUSTOM_STORAGE_DIRECTORY)
 
 pool = Pool(os.cpu_count())
-pool.map(download, download_path)
+pool.map(download, iter_list)
